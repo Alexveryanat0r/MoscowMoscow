068447248f29ad965210f42b75f78111a322b8ba
Alexveryanat0r <sashapozdyshevvv@mail.ru>
2023-11-13 18:51:08 +0300

    uhh

---
+++ app/src/main/java/com/example/moscowmoscow/android_small___1_activity.java
@@ -0,0 +1,61 @@
+
+	 
+	/*
+	 *	This content is generated from the API File Info.
+	 *	(Alt+Shift+Ctrl+I).
+	 *
+	 *	@desc 		
+	 *	@file 		android_small___1
+	 *	@date 		Monday 13th of November 2023 03:33:26 PM
+	 *	@title 		Page 1
+	 *	@author 	
+	 *	@keywords 	
+	 *	@generator 	Export Kit v1.3.figma
+	 *
+	 */
+	
+
+package com.example.moscowmoscow;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+import android.view.View;
+import android.content.Intent;
+
+public class android_small___1_activity extends Activity {
+
+	
+	private View _bg__android_small___1;
+	private View _rectangle_1;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.android_small___1);
+
+		
+		_bg__android_small___1 = (View) findViewById(R.id._bg__android_small___1);
+		_rectangle_1 = (View) findViewById(R.id._rectangle_1);
+	
+		
+		_rectangle_1.setOnClickListener(new View.OnClickListener() {
+		
+			public void onClick(View v) {
+				
+//				Intent nextScreen = new Intent(getApplicationContext(), android_small___2_activity.class);
+//				startActivity(nextScreen);
+			
+		
+			}
+		});
+		
+		
+		//custom code goes here
+	
+	}
+}
+	
+	
